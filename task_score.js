c599fb3bbc8f9d899a738a960d5ee509d116ebb2
Leyla Sadirkhanly <slsedrik@gmail.com>
2023-02-10 00:06:12 +0400

    comitted

---
+++ task_score.js
@@ -0,0 +1,9 @@
+let introduction = 10
+let git = 20
+let js = 30
+let average = (introduction + git + js) / 3
+console.log(average);
+
+let firstName = "Timur"
+let surName = "Aliyev"
+console.log(`Average mark for ${firstName} ${surName} ${average}`);
